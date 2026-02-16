71aee2f70bd9ffe2640468a0a0d8d3382194cd4c
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2026-02-16 13:50:56 +0400

    changed somethings

--- temp_registration_form/serializers.py
+++ temp_registration_form/serializers.py
@@ -5,3 +5,11 @@ class GuideInterestSerializer(serializers.ModelSerializer):
     class Meta:
         model = GuideInterest
         fields = '__all__'
+
+    def validate_email(self, value):
+        """
+        Check that the email is not already in use.
+        """
+        if GuideInterest.objects.filter(email__iexact=value).exists():
+            raise serializers.ValidationError("An interest form with this email address has already been submitted.")
+        return value
