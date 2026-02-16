ea26ee0e92cc5d8f3547a1b870da2808677d68cb
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2026-02-16 14:25:37 +0400

    changed somethings

--- linkler/settings.py
+++ linkler/settings.py
@@ -257,3 +257,26 @@ if FLY_APP_NAME:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
+# Logging configuration to show errors in production
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
